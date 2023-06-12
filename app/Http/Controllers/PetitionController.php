413916c478b79bb4f46db3f018bf5000725a6e2e
abdullah kaamil <akaamil@outlook.com>
2023-06-12 10:04:58 +0300

    Building RestFull APi

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -2,48 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PetitionCollection;
+use App\Http\Resources\PetitionResource;
 use App\Models\Petition;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PetitionController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @return PetitionCollection
      */
     public function index()
     {
-        //
+//        return new PetitionCollection(Petition::all());
+
+        return response()->json(new PetitionCollection(Petition::all()), Response::HTTP_OK);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): PetitionResource
     {
-        //
+        $petition = Petition::create($request->only([
+            'title',
+            'description',
+            'category',
+            'author',
+            'signess',
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Petition $petition)
+    public function show(Petition $petition): PetitionResource
     {
-        //
+        return new PetitionResource($petition);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Petition $petition)
+    public function update(Request $request, Petition $petition): PetitionResource
     {
-        //
+        $petition::update($request->only([
+            'title',
+            'description',
+            'category',
+            'author',
+            'signess',
+        ]));
+
+        return new PetitionResource($petition);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Petition $petition)
+    public function destroy(Petition $petition): \Illuminate\Http\JsonResponse
     {
-        //
+        $petition->delete();
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
